2a33922f95c4e533484fe1b3907d845b272ae0ad
Arafa <ahmed.arafaa@hotmail.com>
2018-09-05 14:47:13 +0300

    Initial Commit

---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  // @Input() valuesFromHome: any;
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+
+  constructor(private authServices: AuthService, private alertfiy: AlertifyService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.authServices.register(this.model).subscribe(() => {
+      this.alertfiy.success('registration successful');
+    }, error => {
+      this.alertfiy.error(error);
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+
+}
